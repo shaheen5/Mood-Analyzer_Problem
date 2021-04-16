be3d9f09855a906583230fea49359074bf56c151
shaheen5 <miyashaheen1995@gmail.com>
2021-04-16 13:55:38 +0530

    Added : Test Code To Check Method Returns Happy Mood

--- test/java/com/moodanalyzer/MoodAnalyserTest.java
+++ test/java/com/moodanalyzer/MoodAnalyserTest.java
@@ -5,10 +5,16 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_whenSad_shouldReturnSad()throws Exception{
+    public void givenMessage_whenSad_shouldReturnSad(){
         MoodAnalyser moodanalyser=new MoodAnalyser();
         String mood=moodanalyser.analyseMood("I am in sad mood");
         Assert.assertEquals("SAD",mood);
+    }
+    @Test
+    public void givenMessage_WhenHappy_ShouldReturnHappy(){
+        MoodAnalyser moodanalyser=new MoodAnalyser();
+        String mood=moodanalyser.analyseMood("I am in any mood");
+        Assert.assertEquals("HAPPY",mood);
     }
 }
 
